2e09a3e6214325593586c003a13c5cf525bcfb83
matkil7 <mateuszkiljan7@gmail.com>
2025-03-11 20:29:54 +0100

    added SpecificationBuilderBase<T> class

---
+++ Specifications/Builders/SpecificationBuilderBase.cs
@@ -0,0 +1,25 @@
+﻿namespace Specifications.Builders;
+
+public class SpecificationBuilderBase<T> : ISpecificationBuilder<T> where T : class
+{
+    private readonly List<Func<T, bool>> _conditions = [];
+    private readonly List<ISpecification<T>> _specifications = [];
+
+    public ISpecificationBuilder<T> Rule(ISpecification<T> spec)
+    {
+        _specifications.Add(spec);
+        return this;
+    }
+
+    public ISpecificationBuilder<T> Rule(Func<T, bool> condition)
+    {
+        _conditions.Add(condition);
+        return this;
+    }
+
+    public bool IsValid(T details)
+    {
+        return _specifications.All(spec =>
+            spec.IsSatisfiedBy(details) && _conditions.All(condition => condition(details)));
+    }
+}

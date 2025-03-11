bbcf362c468ff58bb9acc0c5530b7dc42ff1a880
matkil7 <mateuszkiljan7@gmail.com>
2025-03-11 21:00:57 +0100

    added some rule helper for enum specification

---
+++ Specifications/Rules/Enums/IsSatisfiableEnum.cs
@@ -0,0 +1,13 @@
+﻿namespace Specifications.Rules;
+
+public abstract class IsSatisfiableEnum<T, TModel> : ISpecification<TModel> where T : Enum
+{
+    protected T[] _items { get; init; }
+
+    public IsSatisfiableEnum(T[] items)
+    {
+        _items = items;
+    }
+
+    public abstract bool IsSatisfiedBy(TModel detals);
+}

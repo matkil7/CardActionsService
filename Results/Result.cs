57785e032d70b3d190306e51fd7ebe4d50e61e66
matkil7 <mateuszkiljan7@gmail.com>
2025-03-11 22:50:34 +0100

    added results library

---
+++ Results/Result.cs
@@ -0,0 +1,46 @@
+﻿namespace Results;
+
+public class Result<T> : Result
+{
+    private Result()
+    {
+    }
+
+    public T? Value { get; init; }
+
+    public new static Result<T> Success(T value)
+    {
+        return new Result<T> { Value = value };
+    }
+
+    public new static Result<T> Failure(params string[] errors)
+    {
+        return new Result<T> { Errors = errors };
+    }
+
+    public static implicit operator Result<T>(T value)
+    {
+        return Success(value);
+    }
+}
+
+public class Result
+{
+    protected Result()
+    {
+    }
+
+    public bool IsSuccess => Errors == null;
+
+    public string[] Errors { get; init; }
+
+    public static Result Success()
+    {
+        return new Result();
+    }
+
+    public static Result Failure(params string[] errors)
+    {
+        return new Result { Errors = errors };
+    }
+}
---
+++ Results/ResultExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace Results;
+
+public static class ResultExtensions
+{
+    public static TTarget Match<TSource, TTarget>(
+        this Result<TSource> result,
+        Func<TSource, TTarget> onSuccess,
+        Func<string[], TTarget> onFailure)
+    {
+        return result.IsSuccess ? onSuccess(result.Value) : onFailure(result.Errors);
+    }
+}
